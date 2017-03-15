95d456803494b44dbdc2e7ae179ecfb40404abc0
Krystian Skora-BDP483 <krystian.skora@gmail.com>
2017-03-15 17:31:46 +0100

    Google Machine learning lesson 2

---
+++ google-tutorial/viz.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+# 1. Import and print dataset
+iris = load_iris()
+
+test_idx = [0,50,100]
+
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print(test_target)
+print()
